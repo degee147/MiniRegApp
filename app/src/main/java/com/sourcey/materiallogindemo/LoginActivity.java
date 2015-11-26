f16634f097ae9e2fab8e8a973fe137e5018a12eb
Ken Waribo <kenneth.waribo@cinfores.com>
2015-11-26 14:28:27 +0100

    start screen completely done

--- app/src/main/java/com/sourcey/materiallogindemo/LoginActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/LoginActivity.java
@@ -1,16 +1,11 @@
 package com.sourcey.materiallogindemo;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -21,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
 
    // @InjectView(R.id.input_email) EditText _emailText;
    // @InjectView(R.id.input_password) EditText _passwordText;
-    @InjectView(R.id.btn_login) Button _loginButton;
+    @InjectView(R.id.btn_register) Button _loginButton;
    // @InjectView(R.id.link_signup) TextView _signupLink;
 
     @Override
